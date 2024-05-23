035731b623cb4620c6bf6f534c0056a27f81e3e1
Nmcleon <nmcleon10@gmail.com>
2024-05-23 22:14:55 +0200

    Add firebase utils

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -1,14 +1,20 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyDylr8JiHGVGnksyCrKsAxdb2KX7y9oTDo',
   authDomain: 'penned-2a514.firebaseapp.com',
+  databaseURL: 'https://penned-2a514-default-rtdb.firebaseio.com',
   projectId: 'penned-2a514',
   storageBucket: 'penned-2a514.appspot.com',
   messagingSenderId: '233864443225',
   appId: '1:233864443225:web:2dd9ca807a17b5126a0848',
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
